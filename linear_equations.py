f6856525e84e7083ec592286fb0e3b8ff2005f97
Delor4 <delor4@gmail.com>
2020-10-12 12:04:56 +0200

    integer_small

--- linear_equations.py
+++ linear_equations.py
@@ -10,31 +10,37 @@ class GeneticSystem:
     Genetic algorithms
     by Sebastian Kucharczyk
     """
-    GEN_MIN_VAL = -10
-    GEN_MAX_VAL = 10
+    GENERATIONS = 100000
+    SHOW_DEBUG = False
+    GENS = 8
+    GEN_MIN_VAL = -5
+    GEN_MAX_VAL = 5
     # POPULATION_COUNT # set to even number
-    INDIVIDUALS = 90
-    GENS = 10
-    WORST = 1
-    MUTATED_GENS = 8
+    INDIVIDUALS = 1000
+    WORST = int(0.1 * INDIVIDUALS)
+    CROSSED = int(0.99 * (INDIVIDUALS - WORST))
+    MUTATED_GENS = int(0.01 * INDIVIDUALS)
     MATRIX_A = [
-        [-1, 3, -9, 5, 0, 2, 3, 9, 3, -7],
-        [-9, -4, 0, 4, -3, 2, 5, -2, -5, -6],
-        [-4, 3, 6, 5, -1, -6, 6, -8, -4, -2],
-        [-5, -5, 0, -3, 1, 0, 3, 7, 2, 6],
-        [-1, -5, -4, -3, 5, -2, 5, 9, 6, 4],
-        [-8, -3, 9, -3, 2, 4, -6, -9, 0, -7],
-        [6, -8, 9, 2, 1, 1, 6, 8, 1, 9],
-        [2, 1, -8, -8, -1, -3, 3, 9, 6, 7, ],
-        [4, -9, 3, -3, 6, -4, 8, -1, 3, 1],
-        [7, 2, 7, -5, 0, -5, 2, 8, 5, 6],
-        [7, 2, 7, -5, 0, -5, 2, 8, 5, 6],
-        [2, 1, 5, 2, -4, 3, 1, 1]
+        [3, -3, 3, -4, 2, -1, 3, 1],
+        [-3, 1, 0, 2, 3, 1, 3, -4],
+        [4, -1, -1, -4, 1, -2, 4, -2],
+        [0, -3, 1, 0, 3, 0, 2, 1],
+        [-4, -3, 1, -2, 4, 1, 3, 4],
+        [4, -3, 2, 4, -4, -4, 0, -3],
+        [1, -4, 1, 1, 3, 2, 1, -1],
+        [3, -3, 3, -4, 1, -1, 3, 1],
     ]
-    MATRIX_B = [- 8, - 5, 5, - 7, - 9, 0, 0, - 6, 4, - 5, - 5, ]
+    MATRIX_B = [-34,
+                -19,
+                -26,
+                -21,
+                -25,
+                -19,
+                -18,
+                -33, ]
 
     # result: -3,20	2,58	3,01	-1,05	1,01	0,54	4,63	-3,34
-    #1	8	1	-6	9	9	7	0	-8	1
+    # 2	4	-3	2	-1	3	-3	3
     def __init__(self):
         random.seed()
         self.population = self.make_population()
@@ -47,7 +53,7 @@ class GeneticSystem:
         return sum(abs_b_minus_br)
 
     def make_population(self, individuals=INDIVIDUALS):
-        return [self.make_random_individual() for i in range(individuals)]
+        return [self.make_random_individual() for _ in range(individuals)]
 
     def _make_ind(self, gens):
         return {
@@ -60,19 +66,18 @@ class GeneticSystem:
         return int(random.randrange(min_val * 10, max_val * 10) / 10)
 
     def make_random_individual(self, gens=GENS):
-        return self._make_ind([self.randomize_gen() for i in range(gens)])
+        return self._make_ind([self.randomize_gen() for _ in range(gens)])
 
     def sort_pop(self):
-        try:
-            self.population.sort(key=lambda ind: ind['fitness'])
-        except:
-            print(self.population)
+        self.population.sort(key=lambda ind: ind['fitness'])
 
     def remove_worst(self, removed=WORST):
-        self.population = self.population[:-removed]
+        if removed > 0:
+            self.population = self.population[:-removed]
 
     def add_missing(self):
-        # self.population.extend([self._make_ind(self.population[i]['gens']) for i in range(self.INDIVIDUALS - len(self.population))])
+        # self.population.extend([self._make_ind(self.population[i]['gens'])
+        #   for _ in range(self.INDIVIDUALS - len(self.population))])
         while len(self.population) < self.INDIVIDUALS:
             self.population.append(self.make_random_individual())
         # self.population.extend(self.population[self.INDIVIDUALS - len(self.population)])
@@ -89,26 +94,27 @@ class GeneticSystem:
         return self._make_ind(gen1), self._make_ind(gen2)
 
     def cross_pop(self):
-        for i in range(0, len(self.population) & ~1, 2):
-            self.population[i], self.population[i + 1] = self.cross_ind(self.population[i], self.population[i + 1])
+        for _i in range(0, self.CROSSED & ~1, 2):
+            self.population[_i], self.population[_i + 1] = self.cross_ind(self.population[_i], self.population[_i + 1])
 
     def mutate_ind(self, ind):
         ind['gens'][random.randrange(len(ind))] = self.randomize_gen()
         return self._make_ind(ind['gens'])
 
     def mutate_pop(self, mutated_gens=MUTATED_GENS):
-        for i in range(mutated_gens):
+        for _ in range(mutated_gens):
             index = random.randrange(len(self.population))
             self.population[index] = self.mutate_ind(self.population[index])
 
-    def show_ind(self, msg, ind):
+    @staticmethod
+    def show_ind(msg, ind):
         print(msg, ind)
 
     def debug(self, msg):
-        # print(msg)
-        # for i in range(len(self.population)):
-        #   self.show_ind(i, self.population[i])
-        None
+        if self.SHOW_DEBUG:
+            print(msg)
+            for _i in range(len(self.population)):
+                self.show_ind(_i, self.population[_i])
 
     def calculate_new_generation(self):
         self.remove_worst()
@@ -126,48 +132,83 @@ class GeneticSystem:
         return self.population[0]['fitness']
 
     def overall_fitness(self):
-        s = sum([i['fitness'] for i in self.population])
+        s = sum([_i['fitness'] for _i in self.population])
         return s
 
 
-def make_fig():
-    plt.plot(x, y, '', x, y2)
-    plt.title('Alg. genetyczny')
-    plt.xlabel("Pokolenie")
-    plt.ylabel("Wsp. dostosowania")
-    # plt.annotate('początek', xy=(0, 0), xytext=(0.5, -0.5),
-    # arrowprops=dict(facecolor='black', shrink=0.05),
-    # )
-    # plt.annotate('koniec', xy=(x[-1], y[-1]), xytext=(x[-1]*.8, 0.5),
-    # arrowprops=dict(facecolor='black', shrink=0.05),
-    # )
-    plt.legend(['f(x)=wsp. dost', 'f(x)=avg'])
+class Fig:
+    MAX_TO_SHOW = 2000
+
+    def __init__(self):
+        plt.ion()  # enable interactivity
+        self.fig = plt.figure()  # make a figure
+
+        self.x = []
+        self.y = []
+        self.y2 = []
+
+        self.y_sum = 0
+
+        self.x_min = 0
+        self.y_min = 999999
+
+        self.actual_generation = 0
+        self.actual_ind = None
+
+    def store_results(self, generation, best_ind):
+        best_fit = best_ind['fitness']
+        self.actual_generation = generation
+        self.actual_ind = best_ind
+
+        self.x.append(generation)
+        self.y.append(best_fit)
+
+        self.y_sum += best_fit
+        self.y2.append(self.y_sum / generation)
+
+    def check_newbest(self):
+        best_fit = self.y[-1]
+        if self.y_min > best_fit:
+            self.y_min = best_fit
+            self.x_min = self.actual_generation
+            print("New best: ", self.actual_ind)
+
+    def get_avg(self):
+        return self.y2[-1]
+
+    def show_fig(self):
+
+        if self.actual_generation % 100 == 0:
+            self.x = self.x[-self.MAX_TO_SHOW:]
+            self.y = self.y[-self.MAX_TO_SHOW:]
+            self.y2 = self.y2[-self.MAX_TO_SHOW:]
+            drawnow(self.make_fig)
+
+    def make_fig(self):
+        plt.plot(self.x, self.y, '', self.x, self.y2)
+        plt.title('Alg. genetyczny')
+        plt.xlabel("Pokolenie")
+        plt.ylabel("Wsp. dostosowania")
+        plt.legend(['wsp. dost', 'średnia'])
 
 
 if __name__ == "__main__":
     pop = GeneticSystem()
-    plt.ion()  # enable interactivity
-    fig = plt.figure()  # make a figure
-    x = []
-    y = []
-    y_sum = 0
-    y2 = []
+    fig = Fig()
     print("Adjustment factor on start: {}".format(pop.best_fitness()))
-    for i in range(1, 100000):
+    for i in range(1, pop.GENERATIONS):
         pop.calculate_new_generation()
-        if (i % 10 ** int(math.log(i, 10)) == 0):
+        fig.store_results(i, pop.population[0])
+        if i % 10 ** int(math.log(i, 10)) == 0:
             print("Adjustment factor on {} generation: {:.2f}, all: {:.2f}".format(i,
                                                                                    pop.best_fitness(),
                                                                                    pop.overall_fitness()),
-                  pop.population[0]['gens'])
+                  pop.population[0]['gens'],
+                  "avg.:", fig.get_avg()
+                  )
         if pop.best_fitness() < 0.5:
+            print("Find: ", pop.population[0], 'after:', i)
             break
-        x.append(i)
-        y.append(pop.best_fitness())
-        y_sum += pop.best_fitness()
-        y2.append(y_sum / i)
-        if i % 100 == 0:
-            x = x[-2000:]
-            y = y[-2000:]
-            y2 = y2[-2000:]
-            drawnow(make_fig)
+
+        fig.check_newbest()
+        fig.show_fig()
